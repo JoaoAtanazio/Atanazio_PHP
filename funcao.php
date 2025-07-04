dec6645cd34a74944623969404c6b4047466e7ce
João Vitor Atanazio <joao_atanazio@estudante.sesisenai.org.br>
2025-07-04 16:38:39 -0300

    atividades PHP dia 04/07/2025
    atividades com tempo, função, declaração de variáveis e echo

---
+++ funcao.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = "Stefanie Hatcher";
+        $length = strlen($name);
+        $cmp = strcmp($name, "Brian Le");
+        $index = strpos($name, "e");
+        $first = substr($name, 9, 5);
+        $name = strtoupper($name);
+        echo $name."<br>";
+        echo $first."<br>";
+        echo $cmp."<br>";
+        echo $length."<br>";
+        echo $name."<br>";
+        echo $index;"<br>"
+    ?>
+</body>
+</html>
